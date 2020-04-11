ab8f00fcc309875e3fbfd3a360a68b02c511a7d6
TranNhutKhaKHTN <trannhutkha123456@gmail.com>
2020-04-11 22:03:17 +0700

    first commit

---
+++ Source.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+
+void nhap(int &n);
+void nhap(int& n) {
+    do {
+        printf("\nNhap n ");
+        scanf_s("%d", &n);
+    } while (n < 5);
+}
+
+void ngto(int& n);
+void ngto(int& n) {
+    int j, i, S1 = 1;
+    for (j = 1; j <= n; ++j)
+    {
+        int count = 0;
+        for (i = 2; i <= sqrt(j); i++)
+        {
+            if (j % i == 0)
+            {
+                count++;
+            }
+        }
+        if (count == 0 && j > 1) {
+            S1 = S1 * j;
+        }
+    }printf("\nS1 = %d", S1);
+}
+
+int main()
+{
+    int n;
+    nhap(n);
+    printf_s("%d", n);
+    ngto(n);
+    return 0;
+}
